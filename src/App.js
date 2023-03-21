69f5621b87217ef575b54d1b79530b74e65b1cb7
Suresh <jsithsuresh@gmail.com>
2023-03-22 01:37:17 +0530

    Add FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+import humanIcon from './public/Images/Human.png'
+import robotIcon from './public/Images/Robot.png'
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <div className='main'>
+        <div className='image-div'>
+          <img src={humanIcon} width={70} height={70} alt='human' className='human-avtar'/>
+          <p className='human-text'>Hi ChatGPT</p>
+        </div>
+        <div className='robot-div'>
+          <img src={robotIcon} width={70} height={70} alt='chatAI' className='robot-avtar'/>
+          <p className='ai-text'>Hello Human</p>
+        </div>
+      </div>
+      <div className='inputBox'>
+        <input className="inputField" required></input>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
